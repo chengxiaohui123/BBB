c1579398c45a464f30a6b0581692174fadb126b0
Administrator <Administrator@Win10-2020OURCD>
2021-02-19 18:52:04 +0800

    添加项目文件。

---
+++ ClassLibrary2/Interface1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    interface Interface1<T> where T : class
+    {
+            IEnumerable<T> GetAll(int PageIndex,int PageSize,out int Total);
+            T Get(Func<T, bool> predicate);
+            void Add(T entity);
+            void Update(T entity);
+            void Delete(T entity);
+    }
+}
